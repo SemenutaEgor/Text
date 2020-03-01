5d5eb63af516a99a7ebd18c81ae839556f320e4c
SemenutaEgor <e.semenuta@yandex.ru>
2020-03-01 19:00:31 +0300

    First commit

---
+++ Text/Text/ttextlink.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class TTextLink {
+public:
+	TTextLink *pNext, *pDown;
+	char str[81];
+	//Конструктор
+	TTextLink(char *c = NULL, TTextLink *pn = NULL, TTextLink *pd = NULL) {
+		pNext = pn;
+		pDown = pd;
+		if (c == NULL)
+			str[0] = '\0';
+		else
+			strcpy(str, c);
+	}
+};
