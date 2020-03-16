4ac596c576a3cda15a142acb8fbadce246cea414
SemenutaEgor <e.semenuta@yandex.ru>
2020-03-16 13:37:59 +0300

    tests works, new main

--- Text/TextTest/test.cpp
+++ Text/TextTest/test.cpp
@@ -1,6 +1,20 @@
 #include "pch.h"
+#include "../Text/ttext.h"
+#include "../Text/ttextlink.h"
+#include <string>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+TEST(TextTest, can_create_text)
+{
+	ASSERT_NO_THROW(TText t);
+}
+
+TEST(TextTest, can_add_string_into_current_position)
+{
+	TText t;
+	string istr = "test string";
+	//t.SetLine(istr);
+	//t.InsNextLine(istr);
+	ASSERT_NO_THROW(t.InsNextLine(istr));
 }
