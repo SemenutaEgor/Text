a331fc3f823532041ab1f054a3f5a4e6709e25ce
SemenutaEgor <e.semenuta@yandex.ru>
2020-03-23 19:17:21 +0300

    added navigation methods, fixed InsDownLine and DelDown

--- Text/Text/main.cpp
+++ Text/Text/main.cpp
@@ -1,8 +1,17 @@
+//main.cpp
+
+#include <iostream>
 #include <string>
 #include "ttext.h"
+
+using namespace std;
 
 int main() {
 	TText t;
 	t.Read("textin.txt");
 	t.Write("textout.txt");
+	cout << endl << "printing iterator" << endl;
+	for (t.Reset(); t.IsEnd(); t.GoNext()) {
+		cout << t.GetLine() << endl;
+	}
 }
